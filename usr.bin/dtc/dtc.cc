2c323fbbb858d012f3995bf26210909f4a776ecf
Ryan Libby <rlibby@FreeBSD.org>
2017-07-28 15:47:10 -0700

    Revert "dtc: add missing includes"
    This reverts commit e7760ae272fb9baebd62eace15bc10d51e8324bb.

--- usr.bin/dtc/dtc.cc
+++ usr.bin/dtc/dtc.cc
@@ -36,7 +36,6 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 #include <unistd.h>
 
--- usr.bin/dtc/fdt.cc
+++ usr.bin/dtc/fdt.cc
@@ -44,7 +44,6 @@
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
