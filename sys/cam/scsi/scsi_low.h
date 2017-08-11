2afb7c1e9bd0e69dfa3f12ff844e9c8bf414c63d
Ryan Libby <rlibby@FreeBSD.org>
2017-08-10 23:59:06 -0700

    scsi_low.h: macro cleanliness

--- sys/cam/scsi/scsi_low.h
+++ sys/cam/scsi/scsi_low.h
@@ -634,26 +634,22 @@ void scsi_low_bus_reset(struct scsi_low_softc *);
 /*************************************************
  * Message macro defs
  *************************************************/
-#define	SCSI_LOW_SETUP_PHASE(ti, phase)			\
-{							\
+#define	SCSI_LOW_SETUP_PHASE(ti, phase)	do {		\
 	(ti)->ti_ophase = ti->ti_phase;			\
 	(ti)->ti_phase = (phase);			\
-}
+} while (0)
 
-#define	SCSI_LOW_SETUP_MSGPHASE(slp, PHASE)		\
-{							\
+#define	SCSI_LOW_SETUP_MSGPHASE(slp, PHASE)	do {	\
 	(slp)->sl_msgphase = (PHASE);			\
-}
+} while (0)
 
-#define	SCSI_LOW_ASSERT_ATN(slp)			\
-{							\
+#define	SCSI_LOW_ASSERT_ATN(slp)	do {		\
 	(slp)->sl_atten = 1;				\
-}
+} while (0)
 
-#define	SCSI_LOW_DEASSERT_ATN(slp)			\
-{							\
+#define	SCSI_LOW_DEASSERT_ATN(slp)	do {		\
 	(slp)->sl_atten = 0;				\
-}
+} while (0)
 
 /*************************************************
  * Inline functions
